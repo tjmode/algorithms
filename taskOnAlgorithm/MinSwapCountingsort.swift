be8bf8774eb1c6016fd2bc0dc90f07f40537ff87
tony wilson <40241323+tjmode@users.noreply.github.com>
2020-05-26 15:09:26 +0530

    Update MinSwapCountingsort.swift

--- taskOnAlgorithm/MinSwapCountingsort.swift
+++ taskOnAlgorithm/MinSwapCountingsort.swift
@@ -1,16 +1,16 @@
 /*
 to sort an array with minimum number of sort
 */
-
 import Foundation
 func countingSort(array: [Int]) -> [Int] {
-    var storedArray = Array(0...array.count)
+    var array = array
+    var storedArray = Array(1...array.count)
     var indexCount = [Int]()
     let indexRange = Array((array.min() ?? 0)...(array.max() ?? 0))
     var sumCount = [Int]()
     var count = 0
-    for i in indexRange {
-        indexCount.append(array.lazy.filter{$0 == i}.count)
+    for occurrenceOfNumber in indexRange {
+        indexCount.append(array.lazy.filter{$0 == occurrenceOfNumber}.count)
         if indexCount.count == 1 {
             sumCount.append(indexCount[count])
         } else if indexCount.count == 2{
@@ -20,11 +20,17 @@ func countingSort(array: [Int]) -> [Int] {
         }
     count += 1
     }
-    for j in array {
-        var temp = sumCount[(indexRange.index(of: j) ?? 0)]
-        storedArray[temp] = j
-        sumCount[(indexRange.index(of: j) ?? 0)] = (sumCount[(indexRange.index(of: j) ?? 0)]) - 1
+    var swapCount = 0
+    for arrayValueAndIndex in array {
+        var temp = sumCount[(indexRange.index(of:  arrayValueAndIndex) ?? 0)]
+        if array[temp - 1] !=  arrayValueAndIndex {
+             storedArray[temp - 1]  =  arrayValueAndIndex
+             swapCount += 1
+        }
+        sumCount[(indexRange.index(of:  arrayValueAndIndex) ?? 0)] = (sumCount[(indexRange.index(of:  arrayValueAndIndex) ?? 0)]) - 1
     }
+    print(swapCount)
     return storedArray
+
 }
-print(countingSort(array: []))
+print(countingSort(array: [5,4,3,2,1]))
