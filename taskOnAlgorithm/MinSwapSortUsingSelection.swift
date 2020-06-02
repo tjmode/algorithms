a6e95dfe27bf0d32f9ba9c44fe1537865ff23f5b
tony wilson <40241323+tjmode@users.noreply.github.com>
2020-06-02 14:48:49 +0530

    Update MinSwapSortUsingSelection.swift

--- taskOnAlgorithm/MinSwapSortUsingSelection.swift
+++ taskOnAlgorithm/MinSwapSortUsingSelection.swift
@@ -1,49 +1,36 @@
 /*
-sort the elements of array with minimum swaps. Also find the sum of difference of values from the given index position in reverse order.
-Inputs: Array size, Array elements, Index position
-output: Sorted array with minimum swap count, sum of difference of values 
+sort the elements of arrayOfElements with minimum swaps. Also find the sum of difference of values from the given index position in reverse order.
+Inputs: arrayOfElements size, arrayOfElements elements, Index position
+output: Sorted arrayOfElements with minimum swap count, sum of difference of values 
 */
+
 import Foundation
-func selectionSort (array: [Int], ascending: String) -> [Int] {
-    var array = array
+func arrayOfElementsSortingWithMinimumSwap (arrayOfElements: [Int]) -> [Int] {
+    var arrayOfElements = arrayOfElements
     var swapCounter = 0
-    var tempArrayMinValue = 0
-    for index in 0..<array.count {
-        let tempArray = array[index...(array.count - 1)]
-        if ascending.lowercased() == "yes" {
-            tempArrayMinValue = tempArray.min() as! Int
-        } else {
-            tempArrayMinValue = tempArray.max() as! Int
-        }
-        if array[index] != tempArrayMinValue {
-            let tempValue = array[index]
-            array[index] = tempArrayMinValue
-            array[(tempArray.index(of:tempArrayMinValue) as! Int )] = tempValue
+    var temparrayOfElementsMinValue = 0
+    for index in 0..<arrayOfElements.count {
+        let temparrayOfElements = arrayOfElements[index...(arrayOfElements.count - 1)]
+        temparrayOfElementsMinValue = temparrayOfElements.min() as! Int
+        if arrayOfElements[index] != temparrayOfElementsMinValue {
+            let tempValue = arrayOfElements[index]
+            arrayOfElements[index] = temparrayOfElementsMinValue
+            arrayOfElements[(temparrayOfElements.index(of:temparrayOfElementsMinValue) as! Int )] = tempValue
             swapCounter += 1
         }
     }
-    print(swapCounter)
-    print(array)
-    return array
+    return arrayOfElements
 }
-func minimumDifference (array: [Int], indexNumber: Int) -> Int {
-    var minimumDifference = 0
-    if array.count > indexNumber {
+func sumOfMinimumDiferenceForGivenRange (arrayOfElements: [Int], indexNumber: Int) -> Int {
+    var sumOfDiference = 0
+    if arrayOfElements.count > indexNumber {
         for index in 0..<indexNumber {
-            minimumDifference = minimumDifference + (array[index + 1] - array[index] )
+            sumOfDiference = sumOfDiference + (arrayOfElements[index + 1] - arrayOfElements[index] )
         }
     }
-    return minimumDifference
+    return sumOfDiference
 }
 
-var array = selectionSort(array: [1,2,1], ascending: "Es")
-let indexnumber = 1
-if array[0] == array.min() ?? 0 {
-    print(minimumDifference(array: array, indexNumber: indexnumber))
-} else {
-
-    array = selectionSort(array: array, ascending: "yes")
-    print(array)
-    print(minimumDifference(array:array, indexNumber: indexnumber))
-}
-
+var sortedArrayOfElements = arrayOfElementsSortingWithMinimumSwap(arrayOfElements: [1,2,1,9])
+print(sortedArrayOfElements)
+sumOfMinimumDiferenceForGivenRange(arrayOfElements: sortedArrayOfElements, indexNumber: 2)
