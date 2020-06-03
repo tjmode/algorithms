5365f98b16f2288794d8283886d4a516c043fd07
tony wilson <40241323+tjmode@users.noreply.github.com>
2020-06-03 12:30:06 +0530

    Create lcmTask.swift

---
+++ taskOnAlgorithm/lcmTask.swift
@@ -0,0 +1,31 @@
+/*
+LCM of two number
+*/
+
+import Foundation
+func largeAndSmallNumberFinder(numberOne: Int , numberTwo: Int) -> Int {
+    if numberOne <= numberTwo {
+       let largeNumber = numberTwo
+       let smallNumber = numberOne
+       return lcmCalculation(largeNumber: largeNumber, smallNumber: smallNumber)
+    } else {
+       let largeNumber = numberOne
+       let smallNumber = numberTwo
+       return lcmCalculation(largeNumber: largeNumber, smallNumber: smallNumber)
+    }
+}
+
+func lcmCalculation (largeNumber: Int, smallNumber: Int) -> Int {
+    var largeNumber = largeNumber
+    if (largeNumber % largeNumber == 0) , (smallNumber%smallNumber == 0) {
+    } else {
+        largeNumber = largeNumber + 1
+        lcmCalculation (largeNumber: largeNumber, smallNumber: smallNumber)
+    }
+     return largeNumber
+}
+func lcm (numberOne: Int, numberTwo: Int) -> Int{
+    let answer = largeAndSmallNumberFinder(numberOne: numberOne , numberTwo: numberTwo)
+    return answer
+}
+print(lcm(numberOne: 2, numberTwo: 4))
